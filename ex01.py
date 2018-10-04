6726887493f418008ad3003a287497d62f43991a
Rahisul Haque <rahisul@icloud.com>
2018-10-04 15:01:18 -0700

    Starting python3 challenge at Oct 4th 3pm

---
+++ ex01.py
@@ -0,0 +1,23 @@
+# ******************************************************************************************* #
+#                                                                                             #
+#                                                                                             #
+#    ex01.py                                                         __    _            __    #
+#                                                        _________ _/ /_  (_)______  __/ /    #
+#    By: rahisul <rahisul@icloud.com>                   / ___/ __ `/ __ \/ / ___/ / / / /     #
+#                                                      / /  / /_/ / / / / (__  ) /_/ / /      #
+#    Created: 2018/10/04 14:47:53 by rahisul          /_/   \__,_/_/ /_/_/____/___,_/_/       #
+#    Updated: 2018/10/04 14:59:50 by rahisul                                                  #
+#                                                                                             #
+# ******************************************************************************************* #
+
+#This is good old Hello World Program 
+
+print("Hello World!")
+print("Hello again!")
+print("I like typing this!")
+print("This is fun.")
+print("Yay! Printing!")
+print("I'd much rather you 'not'.")
+print('I said, "Don\'t touch this".')
+print("Bye")
+print("********************************************")
