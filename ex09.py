e7a66cf15f5ed994384b505712d0fba58e5059e6
Rahisul Haque <rahisul@icloud.com>
2018-10-06 15:52:09 -0700

    one fifth done

---
+++ ex09.py
@@ -0,0 +1,25 @@
+# ******************************************************************************************* #
+#                                                                                             #
+#                                                                                             #
+#    ex09.py                                                         __    _            __    #
+#                                                        _________ _/ /_  (_)______  __/ /    #
+#    By: rahisul <rahisul@icloud.com>                   / ___/ __ `/ __ \/ / ___/ / / / /     #
+#                                                      / /  / /_/ / / / / (__  ) /_/ / /      #
+#    Created: 2018/10/06 15:30:57 by rahisul          /_/   \__,_/_/ /_/_/____/___,_/_/       #
+#    Updated: 2018/10/06 15:39:54 by rahisul                                                  #
+#                                                                                             #
+# ******************************************************************************************* #
+
+days = "Mon Tue Wed Thur Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print("Here are days: ", days)
+print("Here are months: ", months)
+
+print("""
+        Here is 
+        an 
+        example of 
+        multiline 
+        print!"
+        """)
---
+++ ex10.py
@@ -0,0 +1,25 @@
+# ******************************************************************************************* #
+#                                                                                             #
+#                                                                                             #
+#    ex10.py                                                         __    _            __    #
+#                                                        _________ _/ /_  (_)______  __/ /    #
+#    By: rahisul <rahisul@icloud.com>                   / ___/ __ `/ __ \/ / ___/ / / / /     #
+#                                                      / /  / /_/ / / / / (__  ) /_/ / /      #
+#    Created: 2018/10/06 15:42:15 by rahisul          /_/   \__,_/_/ /_/_/____/___,_/_/       #
+#    Updated: 2018/10/06 15:48:04 by rahisul                                                  #
+#                                                                                             #
+# ******************************************************************************************* #
+
+tabby_cat = "\t I am tab."
+persian_cat = "I'm spliting\n line"
+backslash_cat = "I'm \\ a \\ cat" 
+fat_cat = """
+I'll do a list:
+\tCat food,
+\tFishes,
+\tCatnip\n\t* grass"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
